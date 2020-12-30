c099f191b331406c1989783b7f5bfc913f53a768
g-priime <57049481+g-priime@users.noreply.github.com>
2020-12-30 12:26:48 -0700

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -15,6 +15,35 @@ function Cards() {
               label="Adventure"
               path="/services"
             />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the Islands of Bali in a
+               Private Cruise"
+              label="Luxury"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-9.jpg"
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the Islands of Bali in a
+               Private Cruise"
+              label="Luxury"
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the Islands of Bali in a
+               Private Cruise"
+              label="Luxury"
+              path="/services"
+            />
           </ul>
         </div>
       </div>
